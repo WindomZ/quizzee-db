55d33d63eebe117bc471f8e73024c4ea7b045987
WindomZ <git.windomz@gmail.com>
2018-02-26 17:52:21 +0800

    feat: import gkv in db

--- bolt/bolt_test.go
+++ bolt/bolt_test.go
@@ -3,39 +3,57 @@ package bolt
 import (
 	"testing"
 
+	quiz "github.com/WindomZ/quizzee-db"
 	"github.com/WindomZ/testify/assert"
 )
 
-var demo *DB
-var (
-	demoTable = []byte("question")
-	demoKey   = []byte("key")
-	demoValue = []byte("value")
-)
-
-func TestOpen(t *testing.T) {
-	db := Open("../data/test-bolt.db")
-	if v, ok := db.(*DB); ok {
-		demo = v
-	}
+var demoTable = []byte("test_question")
+var demo = &quiz.Quiz{
+	Question: "十万个为什么？",
+	Options: []string{
+		"东1a", "南南2b", "西西西3c", "北北北北4d",
+	},
+	Answer: "中",
 }
 
-func TestRegister(t *testing.T) {
-	assert.NoError(t, demo.Register(demoTable))
+func init() {
+	quiz.Open(demoTable, "../data/test.db")
+}
+
+func TestOpen(t *testing.T) {
+	assert.NoError(t, quiz.Close())
+	assert.NoError(t, quiz.Close())
+	assert.NoError(t, quiz.Open(demoTable, "../data/test.db"))
 }
 
 func TestPut(t *testing.T) {
-	assert.NoError(t, demo.Put(demoTable, demoKey, demoValue))
+	assert.True(t, demo.Completion())
+	assert.NoError(t, demo.Store(demoTable))
 }
 
 func TestGet(t *testing.T) {
-	assert.Equal(t, demoValue, demo.Get(demoTable, demoKey))
+	q := quiz.GetQuiz(demoTable, "   十万个为什么？  ")
+	assert.True(t, q.Completion())
+	assert.Equal(t, demo.Question, q.Question)
+	assert.Equal(t, demo.Options, q.Options)
+	assert.Equal(t, demo.Answer, q.Answer)
+	assert.NotEmpty(t, q.Update)
+}
+
+func TestParseQuiz(t *testing.T) {
+	demo.Update = 0
+	q := quiz.ParseQuiz(demoTable, demo.Bytes())
+	assert.True(t, q.Completion())
+	assert.Equal(t, demo.Question, q.Question)
+	assert.Equal(t, demo.Options, q.Options)
+	assert.Equal(t, demo.Answer, q.Answer)
+	assert.Empty(t, q.Update)
 }
 
 func TestCount(t *testing.T) {
-	assert.Equal(t, 1, demo.Count(demoTable))
+	assert.Equal(t, 1, quiz.Count(demoTable))
 }
 
 func TestClose(t *testing.T) {
-	assert.NoError(t, demo.Close())
+	assert.NoError(t, quiz.Close())
 }
--- db.go
+++ db.go
@@ -1,57 +1,23 @@
 package quizzee_db
 
-import "errors"
-
-type DB interface {
-	Close() error
-	Register([]byte) error
-	Put([]byte, []byte, []byte) error
-	Get([]byte, []byte) []byte
-	Count([]byte) int
-}
-
-var db DB
-
-type Instance func(paths ...string) DB
-
-var inst Instance
-
-func Register(i Instance) {
-	inst = i
-}
+import "github.com/WindomZ/gkv"
 
 func Open(table []byte, paths ...string) error {
-	if inst == nil {
-		return errors.New("forgot to import the driver")
-	}
-	db = inst(paths...)
-	return db.Register(table)
+	return gkv.Open(table, paths...)
 }
 
 func Close() error {
-	if db != nil {
-		return db.Close()
-	}
-	return nil
+	return gkv.Close()
 }
 
 func Put(table, key, value []byte) error {
-	if db == nil {
-		return errors.New("the db service is not started")
-	}
-	return db.Put(table, key, value)
+	return gkv.Put(table, key, value)
 }
 
 func Get(table, key []byte) []byte {
-	if db == nil {
-		return nil
-	}
-	return db.Get(table, key)
+	return gkv.Get(table, key)
 }
 
 func Count(table []byte) int {
-	if db == nil {
-		return 0
-	}
-	return db.Count(table)
+	return gkv.Count(table)
 }
